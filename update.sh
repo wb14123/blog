14079a7c1f4deaf4fc8a4844027fd91d273e9912
Bin Wang <wbin00@gmail.com>
2012-12-06 09:53:54 +0800

    Changed at 2012年 12月 06日 星期四 09:53:54 CST

--- update.sh
+++ update.sh
@@ -4,13 +4,22 @@ BASE_DIR=`pwd`
 J_DIR="./jekyll"
 GIT_DIR="./wb14123.github.com"
 
+push() {
+	git add -A .
+	git commit -m "Changed at $(date)"
+	git push origin master
+}
+
 cd $J_DIR
 jekyll
+
 cd $BASE_DIR
 rm -r $GIT_DIR/*
 cp -r $J_DIR/_site/* $GIT_DIR/
 cd $GIT_DIR
-git add -A .
-git commit -m "Changed at $(date)"
-git push origin master
+push
+
+cd $BASE_DIR
+push
+
 exit $?
