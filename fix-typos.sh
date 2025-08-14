2c2ca1b11b0913c3f332f6b3223dd86315530d70
Bin Wang <bin.wang@mail.binwang.me>
2025-08-14 10:48:10 -0400

    use file to track progress for fixing typos instead of using git

--- fix-typos.sh
+++ fix-typos.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 set -e
-set -x
+
+START_FILE="$1"
 
 for f in `ls -r jekyll/_posts` ; do
+	if [ -n "$START_FILE" ] && [[ "$f" > "$START_FILE" ]]; then
+		echo "Skip $f since it's not after $START_FILE"
+		continue
+	fi
+
 	p="jekyll/_posts/$f"
 	echo "Fixing $f ..."
-	git diff --quiet HEAD -- $p && claude -p "Fix typos in @$p . Never change the file name, links in the article. Skip and exit if it's not an English article."
+	claude -p "Fix typos in @$p . Never change the file name, links in the article. Skip and exit if it's not an English article."
 done
